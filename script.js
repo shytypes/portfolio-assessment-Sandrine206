33021b02df059428bbfb2ac2bf7fa6bb18a4638d
Sandrine206 <sandrinetech@outlook.com>
2025-05-01 21:03:40 +0000

    sign up form added to my portfolio
    The form is functional with 22 as age limit.
      But i couldn't get the layout i thought i have coded.

---
+++ script.js
@@ -0,0 +1,40 @@
+// get and store a ref to forms
+// get and store ref to input
+// return values from input to console
+
+const form = document.querySelector('form')
+
+function handleSubmit(e) {
+  //disables default form submission
+  e.preventDefault()
+
+  // returns various values from input
+  const firstName = document.querySelector('#firstName').value
+  const lastName = document.querySelector('#lastName').value
+  const email = document.querySelector('#email').value
+  const age = document.querySelector('#age').value
+  const resultULView = document.querySelector('#result-view')
+  const ageErrorSpan = document.querySelector('#age-error-span')
+
+  if (age < 22) {
+    alert('You must be 22 years or older to fill this form')
+    ageErrorSpan.innerHTML = `<p style="color red;">${errorMsg}</p>`
+  }
+
+  //store data in an object
+  const formData = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    age: age,
+  }
+  console.log(formData)
+
+  //update our DOM via DOM manipulation
+  resultULView.innerHTML = `<li>first Name: ${formData.firstName}</li>
+  <li> lastName : ${formData.lastName}</li>
+  <li> email: ${formData.email}</li>
+  <li> age : ${formData.age}</li> `
+}
+
+form.addEventListener('submit', handleSubmit)
